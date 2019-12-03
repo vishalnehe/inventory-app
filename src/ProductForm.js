aa9e92424cff729269b6d182a2ebca66ed365fd5
hackerrank <me@hackerrank.com>
2019-12-02 21:24:32 -0800

    fix

--- src/ProductForm.js
+++ src/ProductForm.js
@@ -4,7 +4,6 @@ const RESET_VALUES = {id: '', category: '', price: '', name: ''}
 
 class ProductForm extends Component {
 
-
     constructor(props) {
         super(props);
         this.state = {
@@ -12,8 +11,10 @@ class ProductForm extends Component {
        }
     }
 
-
     validate = () => {
+        let nameError = ""
+        let categoryError = ""
+        let priceError = ""
 
         let name = this.state.product.name;
         let category = this.state.product.category;
@@ -32,7 +33,7 @@ class ProductForm extends Component {
         }
         if(price === ""){
             errors['price'] = "price error"
-            this.props.setState({errors:errors})
+            this.setState({errors:errors})
             return false
         }
         return true;
@@ -41,23 +42,19 @@ class ProductForm extends Component {
     handleSave = (e) => {
 
         if(this.validate()) {
-            console.log("true")
             this.props.onSave(this.state.product);
             this.setState({
                 product: Object.assign({}, RESET_VALUES), errors: {}
             })
         }
-        else{
-            console.log(this.state)
-        }
-
         e.preventDefault();
     }
 
     handleChange = (e) => {
-        const target = e.target
-        const value = target.value
+        const target = e.target    
         const name = target.name
+        const value = target.value
+
         this.setState((prevState) => {
              prevState.product[name] = value
              return { product: prevState.product }
@@ -81,7 +78,7 @@ class ProductForm extends Component {
                         </div>
                         <div className="form-group">
                             <label htmlFor="productPrice">Price</label>
-                            <input className="form-control" id="productPrice" type="number" onChange={this.handleChange} value={this.state.product.price} name="price"></input>
+                            <input className="form-control" id="productPrice" type="text" onChange={this.handleChange} value={this.state.product.price} name="price"></input>
                         </div>
                     
                         <button className="btn btn-primary" type="button" onClick={this.handleSave}>Save</button>
